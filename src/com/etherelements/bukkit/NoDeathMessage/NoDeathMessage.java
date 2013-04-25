3035bb8e8535e0aba6969e4baf4829876d8d7d7d
HereInPlainSight <HereInPlainSight@gmail.com>
2013-04-25 01:56:55 -0400

    Housekeeping on the new machine and updating things I never got around to uploading.

--- src/com/etherelements/bukkit/NoDeathMessage/NoDeathMessage.java
+++ src/com/etherelements/bukkit/NoDeathMessage/NoDeathMessage.java
@@ -1,20 +1,26 @@
 package com.etherelements.bukkit.NoDeathMessage;
 
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NoDeathMessage extends JavaPlugin{
+public class NoDeathMessage extends JavaPlugin implements Listener{
 
 	@Override
-	public void onEnable() {
-		 PluginManager pm = getServer().getPluginManager();
-		 pm.registerEvents(new NoDeathMessageDeathListener(), this);
-		 System.out.println("NoDeathMessage has been enabled.");
+	public void onEnable(){
+		 getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	@Override
-	public void onDisable() {
-           System.out.println("NoDeathMessage has been disabled.");
+	public void onDisable(){
 	}
 
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onPlayerDeath(PlayerDeathEvent event){
+		if (event.getEntity().hasPermission("nodeathmessage.nomessage")){
+			event.setDeathMessage(null);
+		}
+	}
 }
